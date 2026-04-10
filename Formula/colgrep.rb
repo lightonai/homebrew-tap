90220bb320c73c65e98ed869e60000ef3a41492a
Raphael Sourty <raphael.sourty@gmail.com>
2026-04-10 17:54:36 +0200

    Update colgrep to 1.2.0

--- Formula/colgrep.rb
+++ Formula/colgrep.rb
@@ -1,25 +1,25 @@
 class Colgrep < Formula
   desc "Semantic code search powered by ColBERT"
   homepage "https://github.com/lightonai/next-plaid"
-  version "1.1.1"
+  version "1.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/lightonai/next-plaid/releases/download/v1.1.1/colgrep-aarch64-apple-darwin.tar.xz"
-      sha256 "4facd57eb113fbb4f5d21d78bf3a61551844263b8afe62290a56c5f96503e2c7"
+      url "https://github.com/lightonai/next-plaid/releases/download/v1.2.0/colgrep-aarch64-apple-darwin.tar.xz"
+      sha256 "dc23caabd1f2e9e9ebc0ce551d2aa53ecbcbf8199c6f2259cc9e4639375aa500"
     end
 
     on_intel do
-      url "https://github.com/lightonai/next-plaid/releases/download/v1.1.1/colgrep-x86_64-apple-darwin.tar.xz"
-      sha256 "3330f4dbbf95b82087e603407afec11ce76512f0d9abe6cc7411695082496758"
+      url "https://github.com/lightonai/next-plaid/releases/download/v1.2.0/colgrep-x86_64-apple-darwin.tar.xz"
+      sha256 "254dd280fd86a43933f5fabc7beefe32fe5c3a1e3ae22885311908091584b977"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/lightonai/next-plaid/releases/download/v1.1.1/colgrep-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "97a5a605244b3cd85a66dc1e4a59ba0c3b46c5afe368548a28f7cfc6b28b5703"
+      url "https://github.com/lightonai/next-plaid/releases/download/v1.2.0/colgrep-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "0bb54942c28e5a1513d6027816e3f8b8ab7f69cc9d96a36bf128afaff3b84f7d"
     end
   end
 
